9e9ac6eb15a588013a275e2712d801f48b3b4a46
Culganosi <culgan_@hotmail.com>
2022-11-15 20:44:23 -0500

    added proper icon tooltips

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -9,22 +9,23 @@ import {
 const SideBar = () => {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-white dark:bg-gray-900 shadow-lg">
-      <SideBarIcon icon={<GiKnifeFork size="28" />} />
+      <SideBarIcon icon={<GiKnifeFork size="28" />} text={"Home"} />
       <Divider />
-      <SideBarIcon icon={<GiCoffeeCup size="28" />} />
-      <SideBarIcon icon={<GiChickenOven size="28" />} />
-      <SideBarIcon icon={<GiFishCooked size="28" />} />
-      <SideBarIcon icon={<GiHamShank size="28" />} />
+      <SideBarIcon
+        icon={<GiChickenOven size="28" />}
+        text={"Chicken Recipes"}
+      />
+      <SideBarIcon icon={<GiFishCooked size="28" />} text={"Fish Recipes"} />
+      <SideBarIcon icon={<GiHamShank size="28" />} text={"Ham Recipes"} />
+      <SideBarIcon icon={<GiCoffeeCup size="28" />} text={"Beverages"} />
     </div>
   );
 };
 
-const SideBarIcon = ({ icon, text = "tooltip" }) => (
+const SideBarIcon = ({ icon, text }) => (
   <div className="sidebar-icon group">
-    {icon}{" "}
-    <span class="sidebar-tooltip group-hover:scale-100">
-      {(text = "All Recipes")}
-    </span>
+    {icon}
+    <span class="sidebar-tooltip group-hover:scale-100">{text}</span>
   </div>
 );
 
