fd593c648140d42fd07709861aec272e26d17c64
Culganosi <culgan_@hotmail.com>
2022-11-16 17:36:15 -0500

    add state to input

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import Header from "./components/Header";
 import MainContainer from "./components/MainContainer";
 import SideBar from "./components/SideBar";
-import TodoList from "./components/TodoList";
 import Logo from "./components/Logo";
-import FormInput from "./components/FormInput";
 
 export default function App() {
   return (
@@ -12,7 +10,6 @@ export default function App() {
       <div className="main-container">
         <Header />
         <Logo />
-        <TodoList />
         {/* <MainContainer /> */}
       </div>
     </div>
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,19 +1,36 @@
 import { useState } from "react";
+import TodoList from "./TodoList";
+import { v4 as uuidv4 } from "uuid";
 
 function FormInput() {
   const [text, setText] = useState("");
-  const [items, setItems] = useState([]);
-  
+  const [listItems, setListItems] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newListItem = {
+      id: uuidv4(),
+      title: text,
+    };
+    setListItems([newListItem, ...listItems]);
+    setText("");
+  };
   return (
     <>
-      <form className="flex items-center justify-center pl-24 mt-10 font-press-start">
+      <form
+        className="flex items-center justify-center pl-24 mt-10 font-press-start"
+        onSubmit={handleSubmit}
+      >
         <input
           type="text"
           name="text"
           placeholder="Add Game"
-          className="py-2 px-5 rounded-lg bg-gray-700"
+          className="py-2 px-5 rounded-lg bg-gray-700 text-green-500"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
       </form>
+      <TodoList />
     </>
   );
 }
